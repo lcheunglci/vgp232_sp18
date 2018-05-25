a860df618d8a7b5388530d7db81dc3783f4b4342
Lawrence Cheung <lcheung@lasallecollegevancouver.com>
2018-05-24 20:10:49 -0700

    [HelloBuild] Add csproj

---
+++ HelloBuild/HelloBuild/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
